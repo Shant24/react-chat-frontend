f84193b5d0f4ee39450b5c42d21adfb3a26d7abb
Shant24 <shant.asf@gmail.com>
2021-05-31 22:35:22 +0400

    fix format date functionality

--- src/helpers/formatDate.ts
+++ src/helpers/formatDate.ts
@@ -1,5 +1,8 @@
 import formatDistanceToNowStrict from 'date-fns/formatDistanceToNowStrict';
 import format from 'date-fns/format';
+import isToday from 'date-fns/isToday';
+import isThisMonth from 'date-fns/isThisMonth';
+import isThisHour from 'date-fns/isThisHour';
 import enUS from 'date-fns/locale/en-US';
 import ru from 'date-fns/locale/ru';
 import hy from 'date-fns/locale/hy';
@@ -13,38 +16,34 @@ const localeArr = {
   hy,
 };
 
-const excludedWordsForShowDate: string[] = ['year', 'month', 'day', 'days', 'лет', 'месяц', 'день'];
-const excludedWordsForShowTime: string[] = ['hour', 'hours', 'час', 'часов'];
+export const formatDateDistance = (date: DateType, isShowTime: boolean = false, locale: LocaleType = 'us'): string => {
+  date = new Date(date);
 
-export const formatDateDistance = (date: DateType, showTime: boolean = false, locale: LocaleType = 'us'): string => {
   let formattedDate = formatDistanceToNowStrict(new Date(date), {
     addSuffix: true,
     locale: localeArr[locale],
     roundingMethod: 'floor',
   });
 
-  // TODO: change functionality
-  excludedWordsForShowDate.forEach((word) => {
-    if (formattedDate.includes(word)) {
-      formattedDate = getDate(date);
-    }
-  });
+  if (isShowTime) {
+    let attemptDate = formattedDate;
 
-  if (showTime) {
-    return `${formattedDate}, ${getTime(date)}`;
+    if (!isThisMonth(date)) {
+      attemptDate = getDate(date);
+    }
+
+    return `${attemptDate}, ${getTime(date)}`;
   }
 
-  excludedWordsForShowTime.forEach((word) => {
-    if (formattedDate.includes(word)) {
-      formattedDate = getTime(date, false);
-    }
-  });
+  if (isThisHour(date)) return formattedDate;
+  if (isToday(date)) return getTime(date, false);
+  if (isThisMonth(date)) return formattedDate;
 
-  return formattedDate;
+  return getDate(date);
 };
 
 export const getDate = (date: DateType) => {
-  return format(new Date(date), 'dd.mm.yyyy');
+  return format(new Date(date), 'dd.MM.yyyy');
 };
 
 export const getTime = (date: DateType, isShowSeconds: boolean = true) => {
