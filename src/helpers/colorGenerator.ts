75cb2a1106c8f3a0094e6afa376ce421a2e90aae
Shant24 <shant.asf@gmail.com>
2021-06-08 01:29:36 +0400

    add new functionality for getting color from username

--- src/helpers/colorGenerator.ts
+++ src/helpers/colorGenerator.ts
@@ -20,6 +20,25 @@ export const generateGradient = ({ color, colorLighten }: { color: string; color
   return `linear-gradient(135deg, ${color} 0%, ${colorLighten} 100%)`;
 };
 
-export const generateAvatarFromName = (name: string): string => {
-  return generateGradient(getColorFromName(name));
+const stringToRGB = (string: string) => {
+  let hash = 0;
+
+  for (let i = 0; i < string.length; i++) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  let c = (hash & 0x00FFFFFF).toString(16);
+  const color = '00000'.substring(0, 6 - c.length) + c;
+
+  return {
+    color: tinycolor(color).toHexString(),
+    colorLighten: tinycolor(color).lighten(40).toHexString(),
+  };
+};
+
+export const generateAvatarFromName = (name: string, type: 'normal' | 'require' = 'require'): string => {
+  const color = type === 'normal' ? getColorFromName(name) : stringToRGB(name);
+  console.log('color', color);
+
+  return generateGradient(color);
 };
