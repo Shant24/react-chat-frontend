06f75db9d831c8cee5849f38b2468d7e1a02dad0
Shant24 <shant.asf@gmail.com>
2021-06-20 20:29:44 +0400

    connect with fake backend

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,17 +1,38 @@
-import React, { memo } from 'react';
+import React, { memo, useEffect, useState } from 'react';
 
 import styles from './styles.module.scss';
 import demo from '../../../demo';
 import { DialoguesBar, ConversationsPart } from '../..';
+import { IDialog } from '../../../types/dialog';
+import { IMessage } from '../../../types/message';
 
-const Home = () => (
-  <div className={styles.homePage}>
-    <div className={styles.container}>
-      <DialoguesBar dialogues={demo.dialogsArr} />
+const Home = () => {
+  const [dialogues, setDialogues] = useState<IDialog[]>(demo.dialogsArr);
+  const [messages, setMessages] = useState<IMessage[]>(demo.chatMessagesArr);
 
-      <ConversationsPart messages={demo.chatMessagesArr} />
+  const getData = async () => {
+    fetch('http://localhost:3003/dialogues')
+      .then((res) => res.json())
+      .then((response) => setDialogues(response));
+
+    fetch('http://localhost:3003/messages')
+      .then((res) => res.json())
+      .then((response) => setMessages(response));
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div className={styles.homePage}>
+      <div className={styles.container}>
+        <DialoguesBar dialogues={dialogues} />
+
+        <ConversationsPart messages={messages} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default memo(Home);
